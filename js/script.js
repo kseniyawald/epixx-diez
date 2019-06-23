c102246b040041abf7c930ce404cb86dbae56293
Kseniya Goncharova <kseniya.wald@yandex.ru>
2019-06-23 19:35:21 +0300

    додела мобилку

--- js/script.js
+++ js/script.js
@@ -24,6 +24,46 @@ $(document).ready(function() {
     $(this).toggleClass('burger--open');
   });
 
-  $('.video__gallery').slick();
+  $('.video__gallery').slick({
+    prevArrow: '<button class="btn--prev"></button>',
+    nextArrow: '<button  class="btn--next"></button>',
+    slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
+  });
+
+  ymaps.ready(function () {
+var myMap = new ymaps.Map('map', {
+center: [59.941993, 30.279423],// center
+zoom: 14,
+controls: []
+}),
+
+// Создаём макет содержимого.
+MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+'<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+),
+
+myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+hintContent: 'Собственный значок метки',
+balloonContent: 'Это красивая метка'
+}, {
+// Опции.
+// Необходимо указать данный тип макета.
+iconLayout: 'default#image',
+// Своё изображение иконки метки.
+iconImageHref: 'images/geotag.png',
+// Размеры метки.
+iconImageSize: [30, 42],
+// Смещение левого верхнего угла иконки относительно
+// её "ножки" (точки привязки).
+iconImageOffset: [-5, -38]
+});
+
+myMap.behaviors.disable('scrollZoom');
+
+myMap.geoObjects
+.add(myPlacemark);
+});
 
 });
